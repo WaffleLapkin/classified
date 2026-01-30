54b7f732a1b6bbc8dc165cb4fdda830422553bad
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-01-30 17:53:30 +0100

    random bulshit go

--- flake.nix
+++ flake.nix
@@ -1,46 +1,47 @@
 {
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
     naersk.url       = "github:nix-community/naersk";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, naersk, systems }:
       let 
-        packageName = "classified";
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        naersk-lib = naersk.lib."${system}";
+       
+        forAllSystems =
+          function:
+          nixpkgs.lib.genAttrs (import systems) (
+            system: function nixpkgs.legacyPackages.${system}
+          );
       in rec {
-        packages.${packageName} = naersk-lib.buildPackage {
-          pname = "${packageName}";
-          root = ./.;
-          postInstall = ''
-            mkdir -p $out/share/{bash-completion/completions,zsh/site-functions,fish/vendor_completions.d}
-            $out/bin/classified completions bash > $out/share/bash-completion/completions/classified.bash
-            $out/bin/classified completions zsh > $out/share/zsh/site-functions/_classified
-            $out/bin/classified completions fish > $out/share/fish/vendor_completions.d/classified.fish
-          '';
-        };
-        defaultPackage = packages.${packageName};
+        packages = forAllSystems (pkgs: rec {
+          classified = naersk.lib.${pkgs.stdenv.hostPlatform.system}.buildPackage {
+            pname = "classified";
+            root = ./.;
+            postInstall = ''
+              mkdir -p $out/share/{bash-completion/completions,zsh/site-functions,fish/vendor_completions.d}
+              $out/bin/classified completions bash > $out/share/bash-completion/completions/classified.bash
+              $out/bin/classified completions zsh > $out/share/zsh/site-functions/_classified
+              $out/bin/classified completions fish > $out/share/fish/vendor_completions.d/classified.fish
+            '';
+          };
+          default = classified;
+        });
+        
+        # apps.${packageName} = packages.${packageName};
+        # defaultApp = apps.${packageName};
 
-        apps.${packageName} = packages.${packageName};
-        defaultApp = apps.${packageName};
+        nixosModules.default = import ./module.nix;
 
-        nixosModules.default = import ./module.nix defaultApp;
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustc
-            cargo
-            clippy
-            rustfmt
-            rust-analyzer
-          ];
-        };
-      }
-    );
+        # devShell = pkgs.mkShell {
+        #   buildInputs = with pkgs; [
+        #     rustc
+        #     cargo
+        #     clippy
+        #     rustfmt
+        #     rust-analyzer
+        #   ];
+        # };
+      };
 }
